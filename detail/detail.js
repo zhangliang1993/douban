f1dcf40a183eca8300f10ea17d0150bae639caf9
zhangliang <1551535499@qq.com>
2016-10-12 19:35:24 +0800

    commit

---
+++ detail/detail.js
@@ -0,0 +1,20 @@
+(function() {
+    var detailModule = angular.module('doubanApp.detail', ['doubanApp.service']);
+    detailModule.config(['$routeProvider', function($routeProvider) {
+        $routeProvider.
+        when('/detail/:movieId', {
+            templateUrl: 'detail/detail.html',
+            controller: 'DetailController'
+        })
+    }])
+    detailModule.controller('DetailController', ['$scope', '$routeParams', 'JsonpService', 'appConfig','$rootScope','$route', function($scope, $routeParams, JsonpService, appConfig,$rootScope,$route) {
+        $scope.loading = false;
+
+        JsonpService.jsonp(appConfig.detaiUrl + $routeParams.movieId, {}, function(res) {
+            // console.log(res)
+            $scope.movie = res;
+            $scope.loading = true;
+            $scope.$apply();
+        })
+    }])
+})();
